56d570d8f4273278cc7bbe274aa228a0accaacaf
Roman Belkov <romanbelkov@gmail.com>
2014-12-06 19:20:49 +0300

    Bad style is gone

--- GeomBuilder/Form1.cs
+++ GeomBuilder/Form1.cs
@@ -13,7 +13,7 @@ namespace GeomBuilder
 {
     public partial class Form1 : Form
     {
-        private Geometry.Point[] Points = null;
+        private Geometry.Point[] points = null;
         public Form1()
         {
             InitializeComponent();
@@ -24,23 +24,24 @@ namespace GeomBuilder
             var rnd = new System.Random();
             Func<int> next = () => rnd.Next(0, 400);
             Func<Geometry.Point> nextPt = () => new Geometry.Point(next(), next());
-            Points = new Geometry.Point[4].Select(i => nextPt()).ToArray();
 
-            var res = Geometry.Checker(Points[0], Points[1], Points[2], Points[3]);
+            points = (from _ in Enumerable.Range(0, 4) select nextPt()).ToArray();
+            var res = Geometry.Checker(points[0], points[1], points[2], points[3]);
+
             label1.Text = res ? "yes" : "no";
             this.Invalidate();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            if (Points != null)
+            if (points != null)
             {
                 e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
 
                 Func<Geometry.Point, Point> convert = (p) => new Point((int)p.X, (int)p.Y);
 
-                e.Graphics.DrawLine(new Pen(Color.Red), convert(Points[0]), convert(Points[1]));
-                e.Graphics.DrawLine(new Pen(Color.CornflowerBlue), convert(Points[2]), convert(Points[3]));
+                e.Graphics.DrawLine(new Pen(Color.Red), convert(points[0]), convert(points[1]));
+                e.Graphics.DrawLine(new Pen(Color.CornflowerBlue), convert(points[2]), convert(points[3]));
             }
         }
     }
