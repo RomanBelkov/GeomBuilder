d880ca63624337fc7e3c859708c2d803795a48f8
Roman Belkov <romanbelkov@gmail.com>
2014-12-19 22:15:52 +0300

    Added tests

---
+++ Tests/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open GeometryLibrary.Geometry
+open NUnit.Framework
+
+[<Test>]
+let ``Simple intersect``() =
+    let a = {X = 0.; Y = 0.}
+    let b = {X = 0.; Y = 5.}
+    let c = {X = -1.; Y = 0.}
+    let d = {X = 3.;  Y = 2.}
+    Assert.AreEqual(true, Checker a b c d)
+
+[<Test>]
+let ``No intersection``() = 
+    let a = {X = 5.; Y = 7.}
+    let b = {X = 10.; Y = 5.}
+    let c = {X = -1.; Y = 0.}
+    let d = {X = 3.;  Y = 2.}
+    Assert.AreEqual(false, Checker a b c d)
+
+[<Test>]
+let ``Bounding box case with intersection``() = 
+    let a = {X = -3.; Y = 3.}
+    let b = {X = 2.; Y = -2.}
+    let c = {X = 0.; Y = 0.}
+    let d = {X = -5.;  Y = 5.}
+    Assert.AreEqual(true, Checker a b c d)
+
+[<Test>]
+let ``Bounding box case without intersection``() = 
+    let a = {X = -7.; Y = -7.}
+    let b = {X = -2.; Y = -2.}
+    let c = {X = 0.; Y = 0.}
+    let d = {X = 5.;  Y = 5.}
+    Assert.AreEqual(false, Checker a b c d)
